d76aea11caa5bfe3c702bad9511b45f32cfb7c67
zempo <zelenkosolomon@gmail.com>
2020-03-07 22:57:03 -0800

    add controllers

--- controllers/transactionControl.js
+++ controllers/transactionControl.js
@@ -1,20 +1,55 @@
+const Transaction = require("../models/Transaction");
+
 // @desc Get all transactions
 // @route GET /api/v1/transactions
 // @access Public
-exports.getTransactions = (req, res, next) => {
-  res.send("GET transactions");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: "Server Error"
+    });
+  }
 };
 
 // @desc Add a transaction
 // @route POST /api/v1/transactions
 // @access Public
-exports.addTransaction = (req, res, next) => {
-  res.send("POST transactions");
+exports.addTransaction = async (req, res, next) => {
+  const { text, amount } = req.body;
+  try {
+    const newTransaction = await Transaction.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: newTransaction
+    });
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map(val => val.message);
+      return res.status(400).json({
+        success: false,
+        error: messages
+      });
+    } else {
+      res.status(500).json({
+        success: false,
+        error: "Server Error"
+      });
+    }
+  }
 };
 
 // @desc Delete a transaction
 // @route GET /api/v1/transactions/:t_id
 // @access Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send("DELETE transactions");
 };
